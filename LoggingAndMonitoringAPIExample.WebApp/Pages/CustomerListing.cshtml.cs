f5b20e987e0f77279a1c9b666ea783e38bbaf441
Münch, Justin <justin.muench@sage.com>
2022-12-14 14:15:14 +0100

    Anpassung Logging

--- LoggingAndMonitoringAPIExample.WebApp/Pages/CustomerListing.cshtml.cs
+++ LoggingAndMonitoringAPIExample.WebApp/Pages/CustomerListing.cshtml.cs
@@ -7,19 +7,25 @@ namespace LoggingAndMonitoringAPIExample.WebApp.Pages
     public class CustomerListingModel : PageModel
     {
         private readonly HttpClient _apiClient;
+        private readonly ILogger<CustomerListingModel> _logger;
         public IEnumerable<CustomerDto> Customers { get; set; }
-        public CustomerListingModel(HttpClient apiClient)
+        public CustomerListingModel(HttpClient apiClient, ILoggerFactory loggerFactory)
         {
             _apiClient = apiClient;
             _apiClient.BaseAddress = new Uri("https://localhost:7081/");
+            _logger = loggerFactory.CreateLogger<CustomerListingModel>();
         }
 
         public async Task OnGetAsync()
         {
-            var response = await _apiClient.GetAsync("api/CustomerCollections");
+            var response = await _apiClient.GetAsync("api/CustomerCollections/error/customers");
 
             if (!response.IsSuccessStatusCode)
             {
+                var details = await response.Content.ReadFromJsonAsync<ProblemDetails>() ?? new ProblemDetails();
+
+                _logger.LogWarning("API failure: {adress} Response {response}, Trace {trace}",
+                    $"{_apiClient.BaseAddress}api/CustomerCollections", response.StatusCode, details.Extensions["traceId"]?.ToString());
                 throw new Exception("Api call failed");
             }
 
